0c3dbbbc9e975d03bb0de5faf4c34a1dbadef61e
friendofthai <friendofthai@gmail.com>
2019-07-02 12:45:25 +0700

    v1.0 release

---
+++ CSPICEiOS/CSPICE/CSPICE.h
@@ -0,0 +1,13 @@
+//
+//  CSPICE.h
+//  CSPICE
+//
+//  Created by Mikhail Krasnorutsky on 01/07/2019.
+//  Copyright © 2019 Mikhail Krasnorutskiy. All rights reserved.
+//
+
+#include <CSPICE/SpiceUsr.h>
+#include <CSPICE/SpiceZfc.h>
+#include <CSPICE/SpiceZmc.h>
+#include <CSPICE/SpiceUsr.h>
+#include <CSPICE/SpiceZdf.h>
